fd14734572ccee6ab0da0ca8ecaadba63f30ae6f
Braian <villasantibraian@icloud.com>
2021-11-09 21:00:40 -0300

    tab scaffold para la pantalla home

---
+++ hp_frontend/lib/screens/home.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(items: [
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.checklist_rounded),
+          label: "Internaciones",
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.contact_page_rounded),
+          label: "Mis pacientes",
+        ),
+        const BottomNavigationBarItem(
+            icon: Icon(Icons.person), label: "Mi información"),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: "Buscar",
+        ),
+      ]),
+      tabBuilder: (BuildContext context, int index) {
+        return CupertinoTabView(
+          builder: (BuildContext context) {
+            return CupertinoPageScaffold(
+              navigationBar: CupertinoNavigationBar(
+                middle: Text('Page 1 of tab $index'),
+              ),
+              child: Center(
+                child: CupertinoButton(
+                  child: const Text('Next page'),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      CupertinoPageRoute<void>(
+                        builder: (BuildContext context) {
+                          return CupertinoPageScaffold(
+                            navigationBar: CupertinoNavigationBar(
+                              middle: Text('Page 2 of tab $index'),
+                            ),
+                            child: Center(
+                              child: CupertinoButton(
+                                child: const Text('Back'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
