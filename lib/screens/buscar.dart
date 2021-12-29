c31c9cc0e0a1d198e511f0173f2982b9f94bf7c2
Braian <villasantibraian@icloud.com>
2021-12-29 09:11:33 -0300

    pantalla buscar

---
+++ lib/screens/buscar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Buscar extends StatelessWidget {
+  const Buscar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Buscar',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 45,
+              child: TextField(
+                keyboardType: const TextInputType.numberWithOptions(),
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search_rounded),
+                  hintText: 'DNI',
+                  isDense: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide: const BorderSide(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide: const BorderSide(
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  fillColor: const Color(0xFFe0e0e2),
+                  filled: true,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hp_frontend/screens/mi_informacion.dart';
 
+import 'buscar.dart';
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -14,7 +16,7 @@ class _HomeState extends State<Home> {
     Container(),
     Container(),
     const MiInformacion(),
-    Container(),
+    const Buscar(),
   ];
   @override
   Widget build(BuildContext context) {
